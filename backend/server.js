8934909c3c1076dd4859eb5b11d619eca7b02008
Tech Savvy <briannyaberi1@.gmail.com>
2022-12-30 15:56:34 +0300

    Mongo db connect

--- backend/server.js
+++ backend/server.js
@@ -6,4 +6,13 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.port || 5000; 
 
+// Connect to DB
+
+mongoose
+.set("strictQuery", false)
+.connect(process.env.MONGO_URL)
+.then(()=> console.log(`Connected to MongoDb`))
+.catch((err) => console.log(err));
+
+
 app.listen(PORT, ()=> console.log(`Listen on: ${PORT}`));
