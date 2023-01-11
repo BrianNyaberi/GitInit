eeb041bb78addf4eec53aa9b6715bbda67394295
Tech Savvy <briannyaberi1@.gmail.com>
2023-01-11 17:26:21 +0300

    React icons, delete and edit

--- frontend/src/components/ToDo.js
+++ frontend/src/components/ToDo.js
@@ -1,11 +1,13 @@
-import React from 'react'
-
 /**
  * Created_by: Brian Nyaberi
  * Created_date: 11-Jan-2023
  * About: Frontend react ToDo app
  * Last Modified_by: 
  */
+
+import React from 'react'
+import {BiEdit} from 'react-icons/bi'
+import {AiFillDelete} from 'react-icons/ai'
 
 const ToDo = ({text, updateMode, deleteToDo}) => {
   return (
